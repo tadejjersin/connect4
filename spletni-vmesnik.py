5a808f4c56a5ecc50501ea4651cd17edebe29d9d
Tadej <tadej.jersin2@gmail.com>
2022-05-14 17:13:34 +0200

    Dodana koda za določanje konca igre in začetek nove

--- spletni-vmesnik.py
+++ spletni-vmesnik.py
@@ -24,14 +24,26 @@ def igra1():
     return bottle.template(
         "igra_za_dva.html",
         na_vrsti = stanje.trenutno_polje_za_2.na_vrsti,
-        mreza = stanje.trenutno_polje_za_2.mreza
+        mreza = stanje.trenutno_polje_za_2.mreza,
+        konec = stanje.trenutno_polje_za_2.preveri_zmago()
     )
 
 @bottle.post("/vrzi_v_stolpec/<id_stolpca:int>/")
 def vrzi_v_prvi_stolpec(id_stolpca):
     st = id_stolpca
     igralec = stanje.trenutno_polje_za_2.na_vrsti
-    stanje.trenutno_polje_za_2.dodaj_potezo(igralec, st)
+    konec = stanje.trenutno_polje_za_2.preveri_zmago()
+    if not konec:
+        stanje.trenutno_polje_za_2.dodaj_potezo(igralec, st)
     bottle.redirect("/igra_za_dva")
+
+@bottle.post("/nova_igra/")
+def nova_igra():
+    stanje.novo_polje_za_2()
+    bottle.redirect("/igra_za_dva")
+
+@bottle.post("/nazaj_na_zacetno_stran/")
+def nazaj_na_zacetno_stran():
+    bottle.redirect("/")
 
 bottle.run(reloader=True, debug=True)
