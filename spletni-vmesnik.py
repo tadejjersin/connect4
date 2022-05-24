4a0fea2904f799e0aad6f6001c005f2f7f8296d5
Tadej <tadej.jersin2@gmail.com>
2022-05-24 10:33:43 +0200

    Koda za dodajanje statistike

--- spletni-vmesnik.py
+++ spletni-vmesnik.py
@@ -38,7 +38,7 @@ def vrzi_v_prvi_stolpec(id_stolpca):
     bottle.redirect("/igra_za_dva")
 
 @bottle.post("/nova_igra/")
-def nova_igra():
+def nova_igra2():
     stanje.novo_polje_za_2()
     bottle.redirect("/igra_za_dva")
 
@@ -47,35 +47,48 @@ def nazaj_na_zacetno_stran():
     bottle.redirect("/")
 
 @bottle.get("/igra_za_enega")
-def igra_za_enega():
+def igra_za_enega(): # statistika se lahko prevečkrat šteje
     na_vrsti = stanje.trenutno_polje_za_1.na_vrsti
     konec, zmagovalec = stanje.trenutno_polje_za_1.preveri_zmago()
-    if konec:
-        stanje.statistika.dodaj_igro(zmagovalec)
-    elif na_vrsti == "igralec2":
+    if na_vrsti == "igralec2" and not konec:
         stolpec = stanje.trenutno_polje_za_1.idealni_stolpec(5)
         stanje.trenutno_polje_za_1.dodaj_potezo("igralec2", stolpec)
     mreza = stanje.trenutno_polje_za_1.mreza
     na_vrsti = stanje.trenutno_polje_za_1.na_vrsti
+    konec, zmagovalec = stanje.trenutno_polje_za_1.preveri_zmago()
+    if konec:
+        stanje.statistika.dodaj_igro(zmagovalec)
+        
     return bottle.template(
         "igra_za_enega.html", 
         na_vrsti = na_vrsti,
         mreza = mreza,
-        konec = stanje.trenutno_polje_za_1.preveri_zmago()[0]
+        konec = konec
     )
 
 @bottle.post("/vrzi_v_stolpec2/<id_stolpca:int>/")
 def vrzi_v_prvi_stolpec(id_stolpca):
     st = id_stolpca
     igralec = stanje.trenutno_polje_za_1.na_vrsti
-    konec = stanje.trenutno_polje_za_1.preveri_zmago()
+    konec = stanje.trenutno_polje_za_1.preveri_zmago()[0]
     if not konec:
         stanje.trenutno_polje_za_1.dodaj_potezo("igralec1", st)
     bottle.redirect("/igra_za_enega")
 
 @bottle.post("/nova_igra1/")
-def nova_igra():
+def nova_igra1():
     stanje.novo_polje_za_1()
     bottle.redirect("/igra_za_enega")
+
+@bottle.get("/statistika")
+def statistika():
+    stat = stanje.statistika.vrni_statistiko()
+    return bottle.template(
+        "statistika.html",
+        st_igr = stat[0],
+        st_izg = stat[1], 
+        st_zm = stat[2],
+        st_neod = stat[3],
+    )
 
 bottle.run(reloader=True, debug=True)
